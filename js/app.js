6bab0e791695d80d5ce75aaf66a5e3b5c1d0a959
Ammon <ammon@tenney.biz>
2016-01-25 11:42:02 -0500

    Welcome screen appears, validates user entered something, and disappears

--- js/app.js
+++ js/app.js
@@ -2,18 +2,33 @@ function AppViewModel() {
     var t = this;
 
     // TODO: load location from localStorage
-    t.location = ko.observable(LoadLocation(false));
+    t.location = ko.observable();
     t.geoLocation = ko.observable();
-    t.locationRadius = ko.observable();
+    t.locationRadius = ko.observable(0);
 
-    if (t.location() === ""){
-        $('.welcome').css('visibility', 'visible');
-    }
-    else {
+    t.welcomeError = ko.observable("&nbsp");
+
+    // if (t.location() === "") $('.welcome').css('visibility', 'visible');
+    // else $('.welcome').css('visibility', 'hidden');
+
+    t.verifyInitialLocation = VerifyInitialLocation(t);
+
+}
+
+function VerifyInitialLocation(t) {
+    return function (){
+        var msg = "";
+
+        if (!t.location())
+            msg = "* Please enter a location";
+
+        if (msg){
+            t.welcomeError(msg);
+            return;
+        }
+
         $('.welcome').css('visibility', 'hidden');
-    }
-
-
+    };
 }
 
 function LoadLocation(tf){
