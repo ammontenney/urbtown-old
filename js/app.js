c0d6f859db4536b063d6b7b20cc1b26cf908bd60
Ammon <ammon@tenney.biz>
2016-01-25 14:34:58 -0500

    Center map on user-provided address

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
+var app = {};
+app.map = {};
+app.geocoder = {};
+app.mapReady = false;
+
+var data = {};
+data.geoLocation = {};
+
 function AppViewModel() {
     var t = this;
 
     // TODO: load location from localStorage
     t.location = ko.observable();
-    t.geoLocation = ko.observable();
     t.locationRadius = ko.observable(0);
 
     t.welcomeError = ko.observable("&nbsp");
@@ -16,15 +23,19 @@ function VerifyInitialLocation(t) {
     return function (){
         var msg = "";
 
-        if (!t.location())
+        if (!t.location()){
             msg = "* Please enter a location";
-
-        if (msg){
             t.welcomeError(msg);
             return;
         }
 
-        // TODO: geolocate the user-provided address
+        if (!app.mapReady){
+            msg = "The map is not ready yet. Please wait 5 seconds and try again.";
+            t.welcomeError(msg);
+            return;
+        }
+
+        orientMap(t.location());
 
         $('.welcome').css('visibility', 'hidden');
     };
@@ -35,14 +46,27 @@ function LoadLocation(tf){
     return "";
 }
 
-var map;
-function initMap(){
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+function orientMap(location){
+    app.geocoder.geocode({'address':location}, function(results, status){
+        if (status == google.maps.GeocoderStatus.OK)
+            app.map.setCenter(results[0].geometry.location);
+        else
+            console.log("Geocode didn't work: " + status);
     });
+
+
 }
 
+
+function NotifyMapIsReady(){
+    app.mapReady = true;
+
+    app.geocoder = new google.maps.Geocoder();
+    app.map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 37.2739675, lng: -104.678212},
+        zoom: 12
+    });
+}
 
 
 
