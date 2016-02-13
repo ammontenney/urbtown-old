35224413fb794dd0b70fdfc318cd1045941e1a00
Ammon <ammon@tenney.biz>
2016-02-13 12:44:12 -0500

    Move global variables into AppViewModel Remove unnecessary global variable: 'app'

--- js/app.js
+++ js/app.js
@@ -1,30 +1,31 @@
-var app = {};
-app.map = {};
-app.geocoder = {};
-app.places = {};
-app.mapReady = false;
 
-var localData = {};
-
-var searchData = {};
-
-var RADIUSVALS = [5,10,15,20,25];
-var CATEGORIES = [
-    {title:'Education', icon:'img/blue.svg', api:'',
-        types:['library', 'school', 'university']},
-    {title:'Medical', icon:'img/red.svg', api:'',
-        types:['dentist', 'doctor', 'health', 'hospital', 'pharmacy', 'physiotherapist']},
-    {title:'Dining', icon:'img/green.svg', api:'',
-        types:['cafe', 'food', 'meal_delivery', 'meal_takeaway', 'restaurant']},
-    {title:'Shopping', icon:'img/gray.svg', api:'',
-        types:['book_store', 'bakery', 'clothing_store', 'convenience_store', 'department_store', 'electronics_store', 'furniture_store', 'grocery_or_supermarket', 'hardware_store', 'home_goods_store', 'pet_store', 'shoe_store', 'shopping_mall', 'store']},
-    {title:'Automotive', icon:'img/yellow.svg', api:'',
-        types:['car_dealer', 'car_rental', 'car_repair', 'car_wash', 'gas_station', 'parking']},
-    {title:'Entertainment', icon:'img/orange.svg', api:'',
-        types:['amusement_park', 'aquarium', 'art_gallery', 'bowling_alley', 'campground', 'movie_rental', 'movie_theater', 'museum', 'park', 'stadium', 'zoo']}
-];
 
 function AppViewModel() {
+    map = {};
+    geocoder = {};
+    places = {};
+    mapReady = false;
+
+    var localData = {};
+
+    var searchData = {};
+
+    var RADIUSVALS = [5,10,15,20,25];
+    var CATEGORIES = [
+        {title:'Education', icon:'img/blue.svg', api:'',
+            types:['library', 'school', 'university']},
+        {title:'Medical', icon:'img/red.svg', api:'',
+            types:['dentist', 'doctor', 'health', 'hospital', 'pharmacy', 'physiotherapist']},
+        {title:'Dining', icon:'img/green.svg', api:'',
+            types:['cafe', 'food', 'meal_delivery', 'meal_takeaway', 'restaurant']},
+        {title:'Shopping', icon:'img/gray.svg', api:'',
+            types:['book_store', 'bakery', 'clothing_store', 'convenience_store', 'department_store', 'electronics_store', 'furniture_store', 'grocery_or_supermarket', 'hardware_store', 'home_goods_store', 'pet_store', 'shoe_store', 'shopping_mall', 'store']},
+        {title:'Automotive', icon:'img/yellow.svg', api:'',
+            types:['car_dealer', 'car_rental', 'car_repair', 'car_wash', 'gas_station', 'parking']},
+        {title:'Entertainment', icon:'img/orange.svg', api:'',
+            types:['amusement_park', 'aquarium', 'art_gallery', 'bowling_alley', 'campground', 'movie_rental', 'movie_theater', 'museum', 'park', 'stadium', 'zoo']}
+    ];
+
     var t = this;
     t.radiusValues = ko.observableArray(RADIUSVALS);
     t.categories = ko.observableArray(CATEGORIES);
@@ -48,7 +49,7 @@ function AppViewModel() {
         // TODO verify the searchRadius value is valid
 
         // Somehow the user managed to enter a location in before the map is ready
-        if (!app.mapReady){
+        if (!mapReady){
             msg = "The map is not ready yet. Please wait 5 seconds and try again.";
             t.welcomeError(msg);
             return;
@@ -113,7 +114,7 @@ function AppViewModel() {
             radius: '5000',
             types: d.types
         };
-        app.places.nearbySearch(request, function(results, status){
+        places.nearbySearch(request, function(results, status){
             if (status !== google.maps.places.PlacesServiceStatus.OK) {
                 console.log("Places search did not work: " + status );
                 return;
@@ -139,7 +140,7 @@ function AppViewModel() {
     };
 
     t.NotifyMapIsReady = function() {
-        app.mapReady = true;
+        mapReady = true;
         // these coordinates default to the center of the USA
         var myLat = 37.2739675;
         var myLng = -104.678212;
@@ -149,17 +150,16 @@ function AppViewModel() {
             myLng = localData.lng;
         }
 
-        app.geocoder = new google.maps.Geocoder();
-        app.map = new google.maps.Map(document.getElementById('map'), {
+        geocoder = new google.maps.Geocoder();
+        map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: myLat, lng: myLng},
             zoom: 13
         });
-        app.places = new google.maps.places.PlacesService(app.map);
+        places = new google.maps.places.PlacesService(map);
     };
 
-
     function centerMap(){
-        app.geocoder.geocode({'address':t.location()}, updateGeo);
+        geocoder.geocode({'address':t.location()}, updateGeo);
         function updateGeo(results, status){
             if (status !== google.maps.GeocoderStatus.OK){
                 console.log("Geocode didn't work: " + status);
@@ -173,7 +173,7 @@ function AppViewModel() {
             localData.lng = results[0].geometry.location.lng();
             saveLocalData();
 
-            app.map.setCenter(results[0].geometry.location);
+            map.setCenter(results[0].geometry.location);
         }
     }
 
@@ -200,15 +200,6 @@ function AppViewModel() {
     else
         $('.welcome').css('visibility', 'visible');
 }
-
-
-
-
-
-
-
-
-
 
 
 var myModel = new AppViewModel();
