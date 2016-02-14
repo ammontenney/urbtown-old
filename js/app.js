c5c3d7e7f0ed78097248308849f265924b025fab
Ammon <ammon@tenney.biz>
2016-02-13 23:11:29 -0500

    Add some TODOs for later

--- js/app.js
+++ js/app.js
@@ -246,6 +246,9 @@ function AppViewModel() {
     }
 
     function resetSearchData(){
+        // TODO: bug-fix - clear markers off screen manually on UpdateLocation
+        // TODO: on UpdateLocation w/ empty query, update locationRadius
+        // TODO: adjust map zoom according to locationRadius
         placeSearchData = {};
     }
 
